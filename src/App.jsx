c343ad235245180a89aa5f1327d0989e54116460
Carlos Ozuna <cozuna@Carloss-MacBook-Air.local>
2026-08-09 19:04:11 -0400

    Clean up unused boilerplate files and images

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import About from './pages/About';
 import Services from './pages/Services';
 import Contact from './pages/Contact';
 import { BookingProvider } from './BookingContext';
-import './App.css';
 
 function App() {
   return (
